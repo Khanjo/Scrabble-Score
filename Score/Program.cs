c3fa0e9b948908373ab554bc9d59f5894fdd2d48
Joshua Khan <Khanjo@gmail.com>
2023-07-12 14:20:54 -0700

    add UI

--- Score/Program.cs
+++ Score/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Scores.Models;
-using Score.UserInterfaceModels;
+using Scores.UserInterfaceModels;
 
 namespace Scores
 {
@@ -9,7 +9,28 @@ namespace Scores
     {
         static void Main()
         {
+            Console.WriteLine(WelcomeBanner.Welcome);
+            Body();
+        }
 
+        static void Body()
+        {
+            Console.WriteLine("Please enter your word and you will see how many points it is worth");
+            string input = Console.ReadLine();
+            Score userScore = new Score(input);
+            userScore.WordArray();
+            int score = userScore.WordScore();
+            Console.WriteLine(score);
+            Console.WriteLine("If you wish to check the score of another word enter 'new' otherwise enter anything to exit program");
+            string userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "new")
+            {
+                Body();
+            }
+            else
+            {
+                Console.WriteLine("Goodbye!");
+            }
         }
     }
 }
